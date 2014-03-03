5546779273dfed211d4e65056cdfbb40ca7bb8f0
Cassidy Williams <cassidoo@outlook.com>
2014-03-03 14:48:12 -0600

    Boo yah, baby.

--- remember.js
+++ remember.js
@@ -1,11 +1,26 @@
 function capture()
 {
     var content = encodeURI(document.getElementById("content").innerHTML);
-    history.replaceState(0,0,"#"+content);
+    history.replaceState(0, 0, "#" + content);
+    // change this to pushState() if you want to clog browser history.  Let's not.
 }
 
 function setup()
 {
     document.getElementById("content").style.height = screen.availHeight + "px";
-    document.getElementById("content").innerHTML = decodeURI(location.hash.substring(1));
+
+    if(decodeURI(location.hash.substring(1)) === "")
+    {
+        document.getElementById("content").innerHTML = "<div id='intro'><h1>welcome to remember. <br> type or paste anything, and share the url with friends.</h1> <button onclick='destroy()'>Got it.</button></div>";
+    }
+    else
+    {
+        document.getElementById("content").innerHTML = decodeURI(location.hash.substring(1));
+    }
 }
+
+function destroy()
+{
+    document.getElementById("content").removeChild(document.getElementById("intro"));
+}
+
